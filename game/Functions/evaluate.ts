a4ed0de3c7be66b7243a5b85546eab471978cd84
Saif Bechan <saifbechan@gmail.com>
2021-02-03 22:51:49 +0100

    printing total fitness score

--- game/Functions/evaluate.ts
+++ game/Functions/evaluate.ts
@@ -15,6 +15,11 @@ const evaluate = (p5: P5, population: Cell[]): Cell[] => {
   // calculate the maximum fitness scores
   const maxFitness: number = Math.max(...fitnessScores);
 
+  // calculate the total fitness score
+  const totalFitness: number = fitnessScores.reduce(
+    (total: number, score: number) => (total += score)
+  );
+
   // normalize the fitness to an integer between 0 and 100
   const normalizedFitnessScores: number[] = fitnessScores.map((score: number) =>
     Math.floor((score / maxFitness) * 100)
@@ -30,6 +35,7 @@ const evaluate = (p5: P5, population: Cell[]): Cell[] => {
 
   console.table({
     'max-fitness': Math.floor(maxFitness),
+    'total-fitness': Math.floor(totalFitness),
     'pool-size': pool.length,
   });
 

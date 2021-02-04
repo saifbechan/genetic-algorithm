fa871c8b07c96938898b972607f230fb5a839ef5
Saif Bechan <saifbechan@gmail.com>
2021-02-04 19:49:09 +0100

    02 - a new generation when all cells are done

--- game/sketch.ts
+++ game/sketch.ts
@@ -14,24 +14,40 @@ enum Population {
 
 const sketch = (p5: P5): void => {
   // our population of cells
-  const cells: Cell[] = populate(p5, Population.Size, Population.Lifespan);
+  let cells: Cell[] = populate(p5, Population.Size, Population.Lifespan);
 
   // our target to go towards
   const target: Target = new Target(p5);
 
+  // the current step (frame) we are in
+  let step = 0;
+
+  // the draw function gets called every frame forever
   p5.draw = (): void => {
     // giving the background a color
     p5.background(11, 0, 20);
 
-    // draw the cells
-    cells.forEach((cell) => {
-      cell.update(target);
+    // we draw when the step is smaller than the lifespan
+    if (step < Population.Lifespan) {
+      // draw the cells
+      cells.forEach((cell) => {
+        cell.update(target);
 
-      cell.draw();
-    });
+        cell.draw();
+      });
 
-    // draw the target
-    target.draw();
+      // draw the target
+      target.draw();
+
+      // increase the steps
+      step += 1;
+    } else {
+      // create a new population
+      cells = populate(p5, Population.Size, Population.Lifespan);
+
+      // reset the steps
+      step = 0;
+    }
   };
 
   p5.setup = (): void => {

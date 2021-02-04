8f5b2102e20fb8f9864ff1d7605214e692c1f187
Saif Bechan <saifbechan@gmail.com>
2021-02-04 20:28:58 +0100

    04 - cells now mutate

--- game/Functions/populate.ts
+++ game/Functions/populate.ts
@@ -3,6 +3,7 @@ import P5 from 'p5';
 
 import DNA from '../ Classes/DNA';
 import Cell from '../Entities/Cell';
+import mutate from './mutate';
 
 const populate = (
   p5: P5,
@@ -21,8 +22,11 @@ const populate = (
     //   - dna created randomly
     const dna = DNA.generate(p5, lifespan, parents);
 
+    // mutate the dna
+    const mutated = mutate(p5, dna);
+
     // create a new cell
-    const cell = new Cell(p5, dna);
+    const cell = new Cell(p5, mutated);
 
     // add our cell to the population
     cells.push(cell);
